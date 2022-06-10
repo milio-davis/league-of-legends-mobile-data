d921460f02f77aa122260893a3259c235af2cf88
milio-davis <emilianograniero@gmail.com>
2022-06-10 19:39:07 -0300

    Added: Splash screen & Login Screen

---
+++ app/src/main/java/com/example/leagueoflegendsapk/activities/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.example.leagueoflegendsapk.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.leagueoflegendsapk.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        /*
+        Login Navigate Button
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+         */
+    }
+}
